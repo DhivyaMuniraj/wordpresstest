e2ad315585401b025b37c805583f36d092787e77
Dhivya Muniraj <dhivya.muniraj@cesltd.com>
2025-02-13 18:16:48 +0530

    added home page about us

---
+++ wp-content/themes/customtheme/contact.php
@@ -0,0 +1,9 @@
+<?php
+/* Template Name: Contact Page */
+get_header();
+?>
+
+<h1>Welcome to the Contact Page</h1>
+<p>If you have any questions, feel free to reach out to us.</p>
+
+<?php get_footer(); ?>
